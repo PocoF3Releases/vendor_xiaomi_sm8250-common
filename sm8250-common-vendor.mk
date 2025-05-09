7264f7a75146da7fac62d25ef96f733ee92deb3a
johnmart19 <johnivan19999@gmail.com>
2025-05-09 15:29:58 +0300

    sm8250-common: Import libstagefrightdolby for Dolby Mediacodecs loading

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -241,6 +241,7 @@ PRODUCT_PACKAGES += \
     libssrec \
     libstagefright_soft_ac4dec \
     libstagefright_soft_ddpdec \
+    libstagefrightdolby \
     libwfdcodecv4l2_proprietary \
     libwfdcommonutils_proprietary \
     libwfdconfigutils_proprietary \
